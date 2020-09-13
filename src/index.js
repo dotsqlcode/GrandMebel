8950888dbdd3a08504d3b1d50e0d77a1dd0b8461
dotsqlcode <dotsqlcode@gmail.com>
2020-09-13 17:01:23 +0300

    Add: created project, created header

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//Libs
+
+import './libs/bootstrap-4.5.0/css/bootstrap-grid.css';
+import './libs/Normalize/Normalize.css';
+
+//Assets
+
+import './img/logo.png';
+
+//Styles
+
+import './less/style.less';
